60e688dcc2bd3abbea9d2f3f3316e917a0a2e86d
Cristiano W. Araújo <cristianowerneraraujo@gmail.comGIT_COMMITTER_EMAIL=cristianowerneraraujo@gmail.com>
2015-04-30 00:46:34 -0300

    Fixing parser issues.

--- naivebayes/parser.py
+++ naivebayes/parser.py
@@ -2,12 +2,15 @@ import re
 from base import Base
 
 class Parser(Base):
-    def __init__(self):
+    def __init__(self, logFile = None):
         self.name = "PARSER"
+        self.logFile = logFile
     def count(self, text):
         self.words = re.compile('\w+').findall(text)
-        self.vocabulary = list(set(word))
-    def getVocabulary():
+        self.vocabulary = list(set(self.words))
+    def getVocabulary(self):
         return self.vocabulary
     def printInfo(self):    
-        self.__print__("Vocabulary size : " + str(len(self.vocabulary)))   
+        self.__print__("Vocabulary size : " + str(len(self.vocabulary)))
+
+Extractor = Parser()
