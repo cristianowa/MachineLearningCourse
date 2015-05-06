f86fbba0be415dc826df912e88ac075c4a92c940
Cristiano W. Araújo <cristianowerneraraujo@gmail.comGIT_COMMITTER_EMAIL=cristianowerneraraujo@gmail.com>
2015-05-06 17:00:38 -0300

    Base now prints time.

--- naivebayes/base.py
+++ naivebayes/base.py
@@ -1,7 +1,10 @@
-
+import time
 class Base:
     def __print__(self, message):
-        msg = self.name + "[ " + message + "]"
+        now = time.localtime()
+        msg = self.name + "[" + str(now.tm_hour) + ":" 
+        msg += str(now.tm_min) + ":" + str(now.tm_sec)
+        msg += "[ " + message + "]"
         if self.logFile == None:
             print msg
         else:
